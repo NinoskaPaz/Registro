b269358404f05207d4a648e2804a925a313f15d1
NinoskaPaz <101575258+NinoskaPaz@users.noreply.github.com>
2022-12-16 02:49:31 -0300

    QR

--- src/app/qr/qr.page.spec.ts
+++ src/app/qr/qr.page.spec.ts
@@ -22,3 +22,4 @@ describe('QrPage', () => {
     expect(component).toBeTruthy();
   });
 });
+
--- src/app/qr/qr.page.ts
+++ src/app/qr/qr.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 
 @Component({
@@ -7,22 +7,69 @@ import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
   templateUrl: './qr.page.html',
   styleUrls: ['./qr.page.scss'],
 })
-export class QrPage implements OnInit {
 
-  ngOnInit() {
+export class QrPage implements OnDestroy{
+
+  qrCodeString = "Usted esta presente";
+  scannerResult: any;
+  content_visibility = 'show';
+
+
+  constructor() {}
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
   }
   
-constructor(private barcodeScanner: BarcodeScanner) { 
+  /*
+  async checkPermission() {
+    try {
+            // check or request permission
+      const status = await BarcodeScanner.checkPermission({ force: true });
 
+      if (status.granted) {
+        // the user granted permission
+        return true;
+      }
 
-    this.barcodeScanner.scan().then(barcodeData => {
-    console.log('Barcode data', barcodeData);
-    }).catch(err => {
-        console.log('Error', err);
-    });
+      return false;
+    } catch(e) {
+      console.log(e);
+    }
+  }
 
+  async startScan() {
+    try{
+      const permission = await this.checkPermission();
+      if(!permission){
+        return;
+      }
+      await BarcodeScanner.hideBackground();
+      document.querySelector('body').classList.add('scanner-active');
+      this.content_visibility = 'hidden';
+      const result = await BarcodeScanner.startScan();
+      console.log(result);
+      if(result?.hasContent){
+        this.scannerResult = result.content;
+        BarcodeScanner.showBackground();
+        document.querySelector('body').classList.remove('scanner-active');
+        console.log(this.scannerResult);
 
-}
+      }
+    }catch(e){
+      console.log(e);
+      this.stopScan();
+    }
+  }
 
+  stopScan(){
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+    document.querySelector('body').classList.remove('scanner-active');
+  }
 
-}
+  ngOnDestroy(): void {
+    this.stopScan();
+  }*/
+
+ }
+
